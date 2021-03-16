c7e14bf8633564805d33192e35b069f3b7785234
Tomi Javanainen <tomi.javanainen@gmail.com>
2021-03-16 16:58:22 +0200

    Update ecosystem.config.js - add a helpful comment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,10 @@
+// FOR USE WITH PM2 TO RUN THIS APP AS A SERVICE
+// https://pm2.keymetrics.io/docs/usage/application-declaration/
+
 module.exports = {
   apps : [{
     script: 'index.js',
     watch: '.',
     ignore_watch: ["pixels.json", "color-images/"]
   }],
-
 };
